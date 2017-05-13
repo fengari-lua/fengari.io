befa827146f5a114aa9738f232b18863e32833a5
Benoit Giannangeli <giann008@gmail.com>
2017-05-13 09:30:58 +0200

    Basic landing page

--- app.lua
+++ app.lua
@@ -1,8 +1,11 @@
 local lapis = require("lapis")
+
 local app = lapis.Application()
+app:enable("etlua")
+app.layout = require "views.layout"
 
 app:get("/", function()
-  return "Welcome to Lapis " .. require("lapis.version")
+    return { render = "index" }
 end)
 
 return app
