020629a7fdba11950763b0643ee46b05fefbb308
Benoit Giannangeli <benoit.giannangeli@boursorama.fr>
2017-05-17 13:59:15 +0200

    Use full-lua repl

--- app.lua
+++ app.lua
@@ -4,7 +4,11 @@ local app = lapis.Application()
 app:enable("etlua")
 app.layout = require "views.layout"
 
-app:get("/", function()
+app:get("/", function(self)
+    local script = io.open("node_modules/fengari-web-cli/src/web-cli.lua")
+    self.repl = script:read("*all")
+    script:close()
+
     return { render = "index" }
 end)
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,17 +7,17 @@ const BabiliPlugin = require("babili-webpack-plugin");
 
 module.exports = [
     {
-        entry: './node_modules/fengari-web-cli/src/web-cli.js',
+        entry: './node_modules/fengari-web-cli/src/web-cli-lua.js',
         output: {
             path: path.resolve(__dirname, 'static/js'),
             filename: 'fengari-web-cli.js',
             library: 'fengari_web_cli'
         },
         plugins: [
-            new BabiliPlugin(),
             new webpack.DefinePlugin({
                 WEB: JSON.stringify(true),
-            })
+            }),
+            new BabiliPlugin()
         ]
     }
 ];
