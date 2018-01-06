fec62996d53d352a0d8134b9dd337181ffece43b
daurnimator <quae@daurnimator.com>
2018-01-07 03:58:07 +1100

    static/lua/snippets.lua: Add Element.closest shim

--- static/lua/snippets.lua
+++ static/lua/snippets.lua
@@ -17,13 +17,32 @@ local printToDOM = function(element, ...)
     end
 end
 
+-- Element.closest shim
+-- https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+local matches = global.Element.prototype.matches
+if not matches then
+    matches = global.Element.prototype.msMatchesSelector or global.Element.prototype.webkitMatchesSelector
+end
+local function closest(el, s)
+    if not document.documentElement:contains(el) then
+        return js.null
+    end
+    repeat
+        if matches(el, s) then
+            return el
+        end
+        el = el.parentElement or el.parentNode
+    until el == js.null
+    return js.null;
+end
+
 -- IE/Edge<=16 doesn't support NodeList.forEach or NodeList[Symbol.iterator]
 local list = document:querySelectorAll(".snippet__actions__action[data-action='run']")
 for i=0, list.length-1 do
     list[i]:addEventListener("click", function (_, event)
         local target = event.currentTarget
         local source = target:querySelector(".snippet__source")
-        local output = target:closest(".snippet"):querySelector(".snippet__output")
+        local output = closest(target, ".snippet"):querySelector(".snippet__output")
 
         local oldPrint = _G.print
 
